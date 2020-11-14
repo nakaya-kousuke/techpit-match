a3dfcb87a37a78dfb17e0a9031f3f97f85eb54d5
nakaya-kousuke <i.my.me.ode23@gmail.com>
2020-11-14 17:36:08 +0900

    like, dislikeをしたときにリクエストを投げるように実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root "top#index"
 
   resources :users, only: [:index, :show]
-  resources :reaction, only: [:create]
+  resources :reactions, only: [:create]
 end
